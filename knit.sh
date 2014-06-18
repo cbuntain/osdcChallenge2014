cfa0b1f341065a73a808cb4d809c782241dd64b4
Nelson Auner <ne.auner@gmail.com>
2014-06-18 11:37:10 +0200

    knit.sh

---
+++ knit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# knit.sh -- Dave Kleinschmidt, April 2013
+# streamline knitting of Rnw files from the command line.
+
+usage="Usage: $0 input-filename.Rnw [-nolatex] [-notangle]"
+
+if [ $# -lt 1 ]; then
+    echo $usage
+    exit 1
+fi 
+
+rnwinput=$1
+shift 
+
+nolatex=0
+notangle=0
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -nolatex) nolatex=1;;
+        -notangle) notangle=1;;
+        -*) echo $usage >&2
+            exit 1;;
+        *) break;;
+    esac
+    shift
+done
+
+
+# first knit Rnw file into
+fileName=${rnwinput%.*}
+
+echo "library(knitr); knit(input='$rnwinput');" | R --no-save --no-restore
+
+if [ $notangle -ne 1 ]; then
+    echo "library(knitr); knit(input='$rnwinput', tangle=T);" | R --no-save --no-restore
+fi
+
+if [ $nolatex -ne 1 ]; then
+    pdflatex ${fileName}.tex && pdflatex ${fileName}.tex
+fi
